469e61e2457bb051482b3fb745108ff1ec07a198
Richard Senar <richardsenar@Richards-MacBook-Air.local>
2017-02-10 13:47:02 -0800

    added functionality

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"os"
+	"path/filepath"
 	"regexp"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
@@ -155,12 +159,37 @@ var usStDict = map[string]string{"AK": "Alaska", "AL": "Alabama",
 	"WA": "Washington", "WI": "Wisconsin", "WV": "West Virginia",
 	"WY": "Wyoming"}
 
+type initConfig struct {
+	CentZip         int
+	MaxRadius       int
+	MaxVehYear      int
+	MinVehYear      int
+	MaxYearDelDate  int
+	MinYearDelDate  int
+	Vendor          string
+	Source          string
+	DelBlankDATE    bool
+	DelBlankDELDATE bool
+}
+
 type payload struct {
 	counter int
 	record  []string
+	param   initConfig
+}
+
+type resources struct {
+	cord   map[string][]string
+	scfFac map[string]string
+	dduFac map[string]string
+	hist   map[string]int
+	dnm    map[string]int
+	genS   map[string]int
+	genSNm map[string]int
 }
 
 func main() {
+	var counter int
 	start := time.Now()
 	var (
 		gophers = flag.Int("gophers", 1, "Workers to run in parallel")
@@ -168,17 +197,28 @@ func main() {
 	)
 	flag.Parse()
 
-	r := csv.NewReader(os.Stdin)
+	reader := csv.NewReader(os.Stdin)
 
 	var colMap map[int]int
+
+	resource := resources{
+		cord:   loadZipCor(),
+		scfFac: loadSCFFac(),
+		dduFac: loadDDUFac(),
+		hist:   loadHist(),
+		dnm:    loadDNM(),
+		genS:   loadGenS(),
+		genSNm: loadGenSNm(),
+	}
 
 	tasks := make(chan payload)
 	go func() {
 		for i := 0; ; i++ {
+			counter = i
 			if i%10000 == 0 {
 				fmt.Print(">")
 			}
-			row, err := r.Read()
+			row, err := reader.Read()
 			if err == io.EOF {
 				break
 			} else if err != nil {
@@ -191,8 +231,9 @@ func main() {
 				})
 			} else {
 				tasks <- mapCol(payload{
-					counter: i,
+					counter: i + 1000,
 					record:  row,
+					param:   loadConfig(),
 				}, colMap)
 			}
 		}
@@ -212,13 +253,340 @@ func main() {
 		go func() {
 			defer wg.Done()
 			for t := range tasks {
-				r := process(t)
+				r := process(t, resource)
 				results <- r
 			}
 		}()
 	}
-	dumpCSV(*outfile, results)
-	fmt.Printf("Elapsed Time: %v\n", time.Since(start).Seconds())
+	outputCSV(*outfile, results)
+	fmt.Printf(" Elapsed Time: %v, Total Records: %v\n", time.Since(start), counter)
+}
+
+func tCase(f string) string {
+	return strings.TrimSpace(strings.Title(strings.ToLower(f)))
+}
+func uCase(f string) string {
+	return strings.TrimSpace(strings.ToUpper(f))
+}
+func lCase(f string) string {
+	return strings.TrimSpace(strings.ToLower(f))
+}
+func cInt(f string) int {
+	i, err := strconv.Atoi(f)
+	if err != nil {
+		log.Fatalln("Error converting string to int", err)
+	}
+	return i
+}
+func decYr(y string) string {
+	if dy, ok := yrDecDict[y]; ok {
+		return dy
+	}
+	return y
+}
+func setSCF(s string) string {
+	switch len(s) {
+	case 5:
+		if s[:1] != "0" {
+			return s[:3]
+		}
+		return s[1:3]
+	case 4:
+		return s[:2]
+	}
+	return ""
+}
+
+func loadConfig() initConfig {
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatalln("Cannot get pwd", err)
+	}
+	rescPath := fmt.Sprintf("/Users/%v/Dropbox/Resource/", strings.Split(pwd, "/")[2])
+
+	conf, err := os.Open(filepath.Join(rescPath, "config.json"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer conf.Close()
+
+	var param initConfig
+
+	jsonParser := json.NewDecoder(conf)
+	if err = jsonParser.Decode(&param); err != nil {
+		log.Fatalln("error decoding json file", err)
+	}
+	return param
+}
+
+func loadZipCor() map[string][]string {
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatalln("Cannot get pwd", err)
+	}
+	rescPath := fmt.Sprintf("/Users/%v/Dropbox/Resource/", strings.Split(pwd, "/")[2])
+
+	cord := make(map[string][]string)
+
+	zipCor, err := os.Open(filepath.Join(rescPath, "USZIPCoordinates.csv"))
+	if err != nil {
+		log.Fatalln("Cannot open ZipCoord file", err)
+	}
+	defer zipCor.Close()
+	rdr := csv.NewReader(zipCor)
+	for {
+		z, err := rdr.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		cord[z[0]] = []string{z[1], z[2]}
+	}
+	return cord
+}
+
+func loadSCFFac() map[string]string {
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatalln("Cannot get pwd", err)
+	}
+	rescPath := fmt.Sprintf("/Users/%v/Dropbox/Resource/", strings.Split(pwd, "/")[2])
+
+	scf := make(map[string]string)
+
+	f, err := os.Open(filepath.Join(rescPath, "SCFFacilites.csv"))
+	if err != nil {
+		log.Fatalln("Cannot open SCFFac file", err)
+	}
+	defer f.Close()
+	rdr := csv.NewReader(f)
+	for {
+		s, err := rdr.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		scf[s[0]] = s[1]
+	}
+	return scf
+}
+
+func loadDDUFac() map[string]string {
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatalln("Cannot get pwd", err)
+	}
+	rescPath := fmt.Sprintf("/Users/%v/Dropbox/Resource/", strings.Split(pwd, "/")[2])
+
+	ddu := make(map[string]string)
+
+	f, err := os.Open(filepath.Join(rescPath, "DDUFacilites.csv"))
+	if err != nil {
+		log.Fatalln("Cannot open DDUFac file", err)
+	}
+	defer f.Close()
+	rdr := csv.NewReader(f)
+	for {
+		s, err := rdr.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		ddu[s[0]] = s[1]
+	}
+	return ddu
+}
+
+func loadHist() map[string]int {
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatalln("Cannot get pwd", err)
+	}
+	rescPath := fmt.Sprintf("/Users/%v/Dropbox/Resource/", strings.Split(pwd, "/")[2])
+
+	hisp := make(map[string]int)
+
+	f, err := os.Open(filepath.Join(rescPath, "HispLNames.csv"))
+	if err != nil {
+		log.Fatalln("Cannot open Hisp file", err)
+	}
+	defer f.Close()
+	rdr := csv.NewReader(f)
+	for {
+		s, err := rdr.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		hisp[tCase(s[0])] = hisp[tCase(s[0])] + 1
+	}
+	return hisp
+}
+
+func loadDNM() map[string]int {
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatalln("Cannot get pwd", err)
+	}
+	rescPath := fmt.Sprintf("/Users/%v/Dropbox/Resource/", strings.Split(pwd, "/")[2])
+
+	dnm := make(map[string]int)
+
+	f, err := os.Open(filepath.Join(rescPath, "DoNotMail.csv"))
+	if err != nil {
+		log.Fatalln("Cannot open DNM file", err)
+	}
+	defer f.Close()
+	rdr := csv.NewReader(f)
+	for {
+		s, err := rdr.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		dnm[tCase(s[0])] = dnm[tCase(s[0])] + 1
+	}
+	return dnm
+}
+
+func loadGenS() map[string]int {
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatalln("Cannot get pwd", err)
+	}
+	rescPath := fmt.Sprintf("/Users/%v/Dropbox/Resource/", strings.Split(pwd, "/")[2])
+
+	gen := make(map[string]int)
+
+	f, err := os.Open(filepath.Join(rescPath, "_GeneralSuppression.csv"))
+	if err != nil {
+		log.Fatalln("Cannot open GenSup file", err)
+	}
+	defer f.Close()
+	rdr := csv.NewReader(f)
+	for {
+		s, err := rdr.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		adrZip := fmt.Sprintf("%v %v", tCase(s[2]), tCase(s[5]))
+		gen[adrZip] = gen[adrZip] + 1
+	}
+	return gen
+}
+
+func loadGenSNm() map[string]int {
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatalln("Cannot get pwd", err)
+	}
+	rescPath := fmt.Sprintf("/Users/%v/Dropbox/Resource/", strings.Split(pwd, "/")[2])
+
+	gen := make(map[string]int)
+
+	f, err := os.Open(filepath.Join(rescPath, "_GeneralSuppressionNames.csv"))
+	if err != nil {
+		log.Fatalln("Cannot open GenSup file", err)
+	}
+	defer f.Close()
+	rdr := csv.NewReader(f)
+	for {
+		s, err := rdr.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fnln := fmt.Sprintf("%v %v", tCase(s[0]), tCase(s[1]))
+		gen[fnln] = gen[fnln] + 1
+	}
+	return gen
+}
+
+func reformatPhone(p string) string {
+	c := []string{"-", " ", ".", "*"}
+	for _, v := range c {
+		p = strings.Replace(p, v, "", -1)
+	}
+	switch len(p) {
+	case 10:
+		p = fmt.Sprintf("(%v) %v-%v", p[0:3], p[3:6], p[6:10])
+	case 7:
+		p = fmt.Sprintf("%v-%v", p[0:3], p[3:7])
+	default:
+		p = ""
+	}
+	return p
+}
+
+func hsin(theta float64) float64 {
+	// haversin(θ) function
+	return math.Pow(math.Sin(theta/2), 2)
+}
+
+func distance(lat1, lon1, lat2, lon2 float64) float64 {
+	// convert to radians, must cast radius as float to multiply later
+	var la1, lo1, la2, lo2, rad float64
+	la1 = lat1 * math.Pi / 180
+	lo1 = lon1 * math.Pi / 180
+	la2 = lat2 * math.Pi / 180
+	lo2 = lon2 * math.Pi / 180
+	rad = 3959 // Earth radius in Miles
+	// calculate
+	h := hsin(la2-la1) + math.Cos(la1)*math.Cos(la2)*hsin(lo2-lo1)
+	return 2 * rad * math.Asin(math.Sqrt(h))
+}
+
+func getLatLong(cZip, rZip string, res resources) (float64, float64, float64, float64) {
+	if cZip[:1] == "0" {
+		cZip = cZip[1:]
+	}
+	if rZip[:1] == "0" {
+		rZip = rZip[1:]
+	}
+	// Set Coordinates Feild
+	recCor, ok := res.cord[rZip]
+	if !ok {
+		log.Fatalln("Invalid Record Zip Code")
+	}
+	// Calculate Distance
+	cenCor, ok := res.cord[cZip]
+	if !ok {
+		log.Fatalln("Invalid Central Zip Code")
+	}
+	lat1, err := strconv.ParseFloat(cenCor[0], 64)
+	lon1, err := strconv.ParseFloat(cenCor[1], 64)
+	lat2, err := strconv.ParseFloat(recCor[0], 64)
+	lon2, err := strconv.ParseFloat(recCor[1], 64)
+	if err != nil {
+		log.Fatalln("Error parsing Coordinates", err)
+	}
+	return lat1, lon1, lat2, lon2
+}
+
+func parseDate(d string) (string, string, string, string) {
+	dfmt := []string{"1/2/06", "01/02/06", "01/02/2006", "1-2-06", "01-02-06", "01-02-2006"}
+	for _, v := range dfmt {
+		t, err := time.Parse(v, d)
+		if err == nil {
+			return t.String()[:10], strconv.Itoa(t.Year()), strconv.Itoa(int(t.Month())), strconv.Itoa(t.Day())
+		}
+	}
+	return "", "", "", ""
 }
 
 func setCol(r payload) map[int]int {
@@ -298,20 +666,72 @@ func mapCol(r payload, m map[int]int) payload {
 	return r
 }
 
-func process(r payload) payload {
-	r.record[custID] = strconv.Itoa(r.counter)
-	r.record[firstName] = "Richard"
-	r.record[lastName] = "Senar"
-	return r
+func process(pay payload, res resources) payload {
+	for i, v := range pay.record {
+		switch i {
+		case state, mi, vin:
+			pay.record[i] = uCase(v)
+		case email:
+			pay.record[i] = lCase(v)
+		case hph, bph, cph:
+			pay.record[i] = reformatPhone(v)
+		default:
+			pay.record[i] = tCase(v)
+		}
+	}
+	// Combine FirstName + LastName to FullName
+	if pay.record[fullName] == "" {
+		pay.record[fullName] = fmt.Sprintf("%v %v", pay.record[firstName], pay.record[lastName])
+	}
+	// Combine address1 + Address2 to AddressFull
+	pay.record[addressFull] = fmt.Sprintf("%v %v", pay.record[address1], pay.record[address2])
+	// Set Phone field based on availability of hph, bph & cph
+	if pay.record[hph] != "" {
+		pay.record[phone] = pay.record[hph]
+	} else if pay.record[bph] != "" {
+		pay.record[phone] = pay.record[bph]
+	} else {
+		pay.record[phone] = pay.record[cph]
+	}
+	// If Zip format is 92882-2341, split to Zip & Zip4
+	if len(pay.record[zip]) == 10 {
+		z := strings.Split(pay.record[zip], "-")
+		pay.record[zip], pay.record[zip4] = z[0], z[1]
+	}
+	// Set VINlen
+	pay.record[vinLen] = fmt.Sprint(len(pay.record[vin]))
+	// Set ZipCrrt
+	pay.record[zipCrrt] = fmt.Sprintf("%v%v", pay.record[zip], pay.record[crrt])
+	// Set Radius(miles) based on Central Zip and Row Zip
+	pay.record[radius] = fmt.Sprintf("%.2f", distance(getLatLong(strconv.Itoa(pay.param.CentZip), pay.record[zip], res)))
+	// Set DelDate, Date, Dld_Year, Dld_Month, Dld_Day
+	pay.record[delDate], pay.record[dldYear], pay.record[dldMonth], pay.record[dldDay] = parseDate(pay.record[delDate])
+	pay.record[date], _, _, _ = parseDate(pay.record[date])
+	// Set SCF value
+	pay.record[scf] = setSCF(pay.record[zip])
+	// Set DDU Faculity
+	if ddufac, ok := res.dduFac[pay.record[zip]]; ok {
+		pay.record[dduFacility] = ddufac
+	}
+	// Set SCF Faculity
+	if scffac, ok := res.scfFac[pay.record[scf]]; ok {
+		pay.record[scf3dFacility] = scffac
+	}
+	// Set Ethnicity
+	if _, ok := res.hist[pay.record[lastName]]; ok {
+		pay.record[41] = "Hisp"
+	}
+
+	return pay
 }
 
-func dumpCSV(out string, results <-chan payload) {
+func outputCSV(out string, results <-chan payload) {
 	f, err := os.Create(out)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer f.Close()
-	w := csv.NewWriter(os.Stdout)
+	w := csv.NewWriter(f)
 	w.Write(header)
 
 	for r := range results {
